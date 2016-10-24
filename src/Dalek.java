f6865665a9d752d623770021f688806bccd5ba57
tatad6701 <tatad6701@HRH-IC0027318.wrdsb.ca>
2016-10-24 14:24:37 -0400

    (Third Commit)

--- src/Dalek.java
+++ src/Dalek.java
@@ -8,25 +8,58 @@
  * @author tatad6701
  */
 public class Dalek {
-    // Create the constants
 
+    // Create the constants
     private int col;
     private int row;
+    private boolean dalekCrash = false;
 
+    /**
+     * A constructor to create the Daleks
+     *
+     * @param theRow The row number of the Dalek character which are randomized
+     * @param theCol The column number of the Dalek character which are
+     * randomized
+     */
     public Dalek(int theRow, int theCol) {
         this.col = theCol;
         this.row = theRow;
     }
 
-//    public advanceTowards(Doctoc doc){        
-// }
+//    public advanceTowards(Doctor doc) {
+//        if () {
+//        }
+//    }
+    /**
+     * Return the row number of the Dalek character
+     *
+     * @return the row number of the Dalek character
+     */
     public int getRow() {
         return this.row;
     }
 
+    /**
+     * Return the column number of the Dalek character
+     *
+     * @return the column number of the Dalek character
+     */
     public int getCol() {
         return this.col;
     }
-//    public boolean hasCrashed(){    
-//    }
+
+    // Create the method here but then add your if statements within the main class 
+    public void crash() {
+        dalekCrash = true;
+    }
+
+    /**
+     * Check if the Dalek has crashed
+     *
+     * @return the
+     * ****************************************************************
+     */
+    public boolean hasCrashed() {
+        return dalekCrash;
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -17,23 +17,75 @@ public class Game {
     public static void main(String[] args) {
         // Create the game board
         Board board = new Board(12, 12);
-        Doctor doctor = new Doctor(6, 6);
-        board.putPeg(Color.GREEN, 6, 6);
-
-//        board.putPeg(Color.red, 1, 5);
-//        board.putPeg(Color.yellow, 2, 6);
-//        board.removePeg(1, 5);
+        // Create the doctor
+        Doctor doctor = new Doctor((int) (Math.random() * 12), (int) (Math.random() * 12));
+        board.putPeg(Color.GREEN, doctor.getRow(), doctor.getCol());
+        // Create the first Dalek
+        Dalek dalek1 = new Dalek((int) (Math.random() * 12), (int) (Math.random() * 12));
+        board.putPeg(Color.BLACK, dalek1.getRow(), dalek1.getCol());
+        // Create the second Dalek
+        Dalek dalek2 = new Dalek((int) (Math.random() * 12), (int) (Math.random() * 12));
+        board.putPeg(Color.BLACK, dalek2.getRow(), dalek2.getCol());
+        // Create tje third Dalek
+        Dalek dalek3 = new Dalek((int) (Math.random() * 12), (int) (Math.random() * 12));
+        board.putPeg(Color.BLACK, dalek3.getRow(), dalek3.getCol());
 
         // Put a message on the board
         board.displayMessage("Please click the board");
 
         // Get a click response from the user
         while (true) {
+            // DOCTOR ACTIONS
+            // Coordinates of the user's click
             Coordinate click = board.getClick();
+            // Remove the old doctor peg
             board.removePeg(doctor.getRow(), doctor.getCol());
+            // Move the doctor peg to new location
             doctor.move(click.getRow(), click.getCol());
+            // Place the doctor peg!
             board.putPeg(Color.GREEN, doctor.getRow(), doctor.getCol());
 
+            // DALEK ACTIONS
+
+
+
+
+
+
+
+
+
+
+
+            // CHECK CRASHES: if Daleks has crashed or not with each other or the Doctor
+            // If Dalek1 has crashed with Dalek2
+            if (dalek1.getRow() == dalek2.getRow() && dalek1.getRow() == dalek2.getRow()) {
+                dalek1.crash();
+                dalek2.crash();
+                // Put a red peg where the two crashed
+                board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+            }
+            // If Dalek 1 has crashed with Dalek3
+            if (dalek1.getRow() == dalek3.getRow() && dalek1.getRow() == dalek3.getRow()) {
+                dalek1.crash();
+                dalek3.crash();
+                // Put a red peg where the two crashed
+                board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+            }
+            // If Dalek 2 and 3 crash
+            if (dalek2.getRow() == dalek3.getRow() && dalek2.getCol() == dalek3.getCol()) {
+                dalek2.crash();
+                dalek3.crash();
+                // Put a red peg where the two crashed
+                board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+            }
+            // If Dalek 1, 2, or 3 has crashed with the doctor // ADD ALL THE DALEKS IN THE IF LOOP!
+            if (dalek1.getRow() == doctor.getRow() && dalek1.getRow() == doctor.getRow()
+                    && dalek2.getRow() == doctor.getRow() && dalek2.getRow() == doctor.getRow()
+                    && dalek3.getRow() == doctor.getRow() && dalek3.getRow() == doctor.getRow()) {
+                // End the game
+                break;
+            }
         }
     }
 }
